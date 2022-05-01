b19afe455344d3f50488cab649222f8e78aea7e1
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-05-01 16:19:44 +0600

    Db conected

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const cors = require('cors');
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors())
 app.use(express.json())
 
 
-app.get('/', (req, res)=>{
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jom4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("monitor-warehouse").collection("products");
+    console.log('DB is connected')
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+app.get('/', (req, res) => {
     res.send('Hello world')
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`example port is runing. ${port}`)
 })

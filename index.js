6afea552416827a38c0410cda1659e4842394b53
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-05-04 15:34:04 +0600

    all products api added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const app = express();
-require('dotenv').config()
 const cors = require('cors');
+const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+// const axios = require('axios');
 
 // middleware 
 app.use(cors())
@@ -11,14 +12,29 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jom4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("monitor-warehouse").collection("products");
-    console.log('DB is connected')
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection= client.db("monitor-warehouse").collection("products");
+
+  
+        app.get('/products', async(req, res)=>{
+            const query={};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray(cursor)
+            res.send(result);
+        })
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 
 

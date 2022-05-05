31d84cd0688f9a107b163f58258a1dbe61622c23
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-05-05 16:20:32 +0600

    delete api create

--- index.js
+++ index.js
@@ -11,33 +11,42 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jom4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
-        const productCollection= client.db("monitor-warehouse").collection("products");
+        const productCollection = client.db("monitor-warehouse").collection("products");
 
-        app.get('/products', async(req, res)=>{
-            const query={};
+        app.get('/products', async (req, res) => {
+            const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray(cursor)
             res.send(products);
         })
-        app.get('/sixproducts', async(req, res)=>{
-            const query={}
+        app.get('/sixproducts', async (req, res) => {
+            const query = {}
             const cursor = productCollection.find(query);
             const products = await cursor.limit(6).toArray();
             res.send(products)
         })
-        app.get('/inventoris/:id', async(req, res)=>{
-            
+        app.get('/inventoris/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const products = await productCollection.findOne(query);
+            res.send(products);
+        })
+        app.delete('/inventoris/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query= {_id:ObjectId(id)};
+            const products= await productCollection.deleteOne(query);
+            res.send(products)
         })
     }
-    finally{
+    finally {
         // await client.close();
     }
 }
